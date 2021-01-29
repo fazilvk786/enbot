ef590be4b50d8815342c4b1a3c86d11612b1e85c
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-01-29 23:17:46 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -4,9 +4,9 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler  = async (m, { conn, text }) => {
   if (text) conn.sendFile(m.chat, 'https://api.xteam.xyz/attp?file&text=' + encodeURIComponent(text), 'attp.webp', '', m, false, { asSticker: true })
-  else throw 'Uhm...Teksnya?'
+  else throw 'Uhm ... The text?'
 }
-handler.help = ['attp <teks>']
+handler.help = ['attp <text>']
 handler.tags = ['sticker']
 handler.command = /^attp$/i
 handler.owner = false
