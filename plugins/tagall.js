2cd3831234e928bcc6d5fa68308c169cfe5d40dc
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-01-29 23:51:20 +0530

    Create tagall.js

---
+++ plugins/tagall.js
@@ -0,0 +1,19 @@
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Language = require('../language');
+const Lang = Language.getString('tagall');
+
+Asena.addCommand({pattern: 'tagall', fromMe: true, desc: Lang.TAGALL_DESC}, (async (message, match) => {
+    grup = await message.client.groupMetadata(message.jid);
+    var jids = [];
+    mesaj = '';
+    grup['participants'].map(
+        async (uye) => {
+            mesaj += '@' + uye.id.split('@')[0] + ' ';
+            jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+        }
+    );
+    await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+}));
