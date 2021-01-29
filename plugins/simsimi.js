08faaf2e4ee5a7de4d46d6425bd4e1de2b6747c8
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-01-29 20:34:34 +0530

    Update simsimi.js

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -4,7 +4,7 @@ let handler = async (m, { text }) => {
   let json = await res.json()
   if (json.status) m.reply(json.jawaban)
 }
-handler.help = ['sim', ''].map(v => v + 'simi <teks>')
+handler.help = ['sim'].map(v => v + 'simi <text>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi)$/i
 
