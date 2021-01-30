01499b9d38db88e8c399faeecae310aba8a40051
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-01-30 17:53:59 +0530

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let { JSDOM } = require('jsdom')
 let limit = 30
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
+  if (!args || !args[0]) return conn.reply(m.chat, 'Uhm ... where's the url', m)
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 *Filesize:* ${filesizeF}
 `.trim(), m)
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ' <url>')
+handler.help = ['mp4','v'].map(v => 'yt' + v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 handler.owner = false
