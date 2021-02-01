b84eaeebaf5070a13182fd21cb0bd9ac8913bda1
Vai838 <75434115+Vai838@users.noreply.github.com>
2021-02-01 12:30:24 +0530

    Update shortener.js

--- plugins/shortener.js
+++ plugins/shortener.js
@@ -1,9 +1,15 @@
-let handler = async (m, { conn }) => {
-  return false
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, args }) => {
+  let full = /f$/i.test(command)
+  if (!args[0]) return conn.reply(m.chat, 'No url', m)
+  let url = /https?:\/\//.test(args[0]) ? args[0] : 'https://' + args[0]
+  let ss = await (await fetch('https://tinyurl.com/api-create.php?url=0' + encodeURIComponent(url) + '&full=' + full)).buffer()
+conn.reply(m.chat,ss, url, m)
 }
-handler.help = [''].map(v => v + ' <>')
-handler.tags = ['']
-handler.command = /^(.{65536})$/i
+handler.help = ['shortener'].map(v => v + ' <url>')
+handler.tags = ['tools']
+handler.command = /^(shortener)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
